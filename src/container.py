17f6d08690a3870239a9745e5d52b52c9b10bb41
winsbar <slavaku2014@gmail.com>
2022-12-04 12:38:00 +0200

    feat(configuration): added application future architecture

---
+++ src/container.py
@@ -0,0 +1,61 @@
+from smtplib import SMTP
+from typing import Generator
+from typing import Callable
+
+from dependency_injector import providers
+from dependency_injector.containers import DeclarativeContainer
+from passlib.context import CryptContext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from src.handlers.container import HandlersContainer
+from src.repositories.container import RepositoriesContainer
+from src.services.container import ServicesContainer
+from src.services.container import Security
+
+
+def database_engine(dsn: str) -> Generator:
+    engine = create_engine(dsn)
+    yield engine
+    engine.dispose()
+
+
+def authorized_smtp_client(host: str, port: int, user: str, password: str) -> Generator:
+    client = SMTP(host, port)
+    if len(user) > 0:
+        client.login(user, password)
+    yield client
+    client.quit()
+
+
+class AppContainer(DeclarativeContainer):
+    config = providers.Configuration()
+    sqlalchemy_engine = providers.Resource(database_engine, dsn=config.postgres_dsn)
+    maker: providers.Singleton = providers.Singleton(sessionmaker, bind=sqlalchemy_engine)
+    scoped_session_provider: providers.Singleton[scoped_session] = providers.Singleton(
+        scoped_session, session_factory=maker)
+    sqlalchemy_session: providers.Singleton[Callable[..., Session]] = providers.Singleton(scoped_session_provider)
+
+    security_context: CryptContext = providers.Factory(CryptContext, schemes=['argon2'])
+    repositories = providers.Container(RepositoriesContainer, sqlalchemy_session=sqlalchemy_session)
+
+    services = providers.Container(
+        ServicesContainer,
+        frontend_url=config.frontend_base_url,
+    )
+
+    security = providers.Container(
+        Security,
+        auth_secret_key=config.auth_secret,
+        auth_token_ttl=config.auth_token_ttl.as_int(),
+    )
+
+    handlers = providers.Container(
+        HandlersContainer,
+        repositories=repositories,
+        services=services,
+        security_context=security_context,
+        security=security,
+    )
